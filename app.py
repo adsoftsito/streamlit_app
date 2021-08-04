51dee0e344bad9c8afbaa56eaaa0d238906fce0f
adsoftsito <adsoft@live.com.mx>
2021-08-04 03:02:11 +0000

    init streamlit

---
+++ app.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import numpy as np
+import CountVectorizer from sklearn.feature_extraction.text
+import DictVectorizer from sklearn.feature_extraction
+
+df = pd.read_csv('dataset.csv')
+df.size
+df.dtypes
+
