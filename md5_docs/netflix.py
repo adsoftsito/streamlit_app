58cc2fab5edc081e1d1c5d8b1af9badd3b02dfb7
adsoftsito <adsoft@live.com.mx>
2021-10-14 06:23:13 +0000

    add netflix 1

--- md5_docs/netflix.py
+++ md5_docs/netflix.py
@@ -7,7 +7,7 @@ import codecs
 
 st.title('Netflix app')
 
-DATE_COLUMN = 'started_at'
+DATE_COLUMN = 'released'
 DATA_URL = ('movies.csv')
 
 @st.cache
@@ -15,25 +15,55 @@ def load_data(nrows):
     doc = codecs.open('movies.csv','rU','latin1')
     data = pd.read_csv(doc, nrows=nrows)
     lowercase = lambda x: str(x).lower()
-    # data.rename(lowercase, axis='columns', inplace=True)
-    # data.rename({'start_lat': 'lat', 'start_lng': 'lon'}, axis=1, inplace=True)
-
-    # data[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
+    data[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
     return data
+
+def filter_data_by_filme(filme):
+    filtered_data_filme = data[data['name'].str.upper().str.contains(filme)]
+    return filtered_data_filme
+
+def filter_data_by_director(director):
+    filtered_data_director = data[data['director'] == director]
+    return filtered_data_director
+
+
+
 
 data_load_state = st.text('Loading cicle nyc data...')
 data = load_data(1000)
 data_load_state.text("Done! (using st.cache)")
 
-if st.sidebar.checkbox('Show raw data'):
-    st.subheader('Raw data')
+if st.sidebar.checkbox('Mostrar todos los filmes'):
+    st.subheader('Todos los filmes')
     st.write(data)
 
-if st.sidebar.checkbox('Recorridos por hora'):
-    st.subheader('Numero de recorridos por hora')
 
-    #hist_values = np.histogram(data[DATE_COLUMN].dt.hour, bins=24, range=(0,24))[0]
-    st.bar_chart(hist_values)
+titulofilme = st.sidebar.text_input('Titulo del filme :')
+btnBuscar = st.sidebar.button('Buscar filmes')
+
+if (btnBuscar):
+   data_filme = filter_data_by_filme(titulofilme.upper())
+   count_row = data_filme.shape[0]  # Gives number of rows
+   st.write(f"Total filmes mostrados : {count_row}")
+   st.write(data_filme)
+
+
+
+selected_director = st.sidebar.selectbox("Seleccionar Director", data['director'].unique())
+btnFilterbyDirector = st.sidebar.button('Filtrar director ')
+
+if (btnFilterbyDirector):
+   filterbydir = filter_data_by_director(selected_director)
+   count_row = filterbydir.shape[0]  # Gives number of rows
+   st.write(f"Total filmes : {count_row}")
+
+   st.dataframe(filterbydir)
+
+#if st.sidebar.checkbox('Filmes por anio'):
+#    st.subheader('Filmes por anio')
+
+#    hist_values = np.histogram(data[DATE_COLUMN].dt.year, range=(1980,2010))[0]
+#    st.bar_chart(hist_values)
 
 
 # Some number in the range 0-23
